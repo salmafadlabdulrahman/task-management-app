38aff5d5e612533af38ac14a599b6d5b19be23fb
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-10-15 21:41:26 +0300

    update style

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -44,7 +44,16 @@ function DashBoard() {
           </div>
         </div>
 
-        <div className="create-new-column">+ New Column</div>
+        <div className="create-new-column"><div className="create-column-btn">+ New Column</div></div>
+
+        <div className="column">
+          <h3 className="column-title">Done (4)</h3>
+
+          <div className="task-card">
+            <h3 className="task-title">Build UI for onboarding flow</h3>
+            <h5 className="task-count">1 of 3 subtasks</h5>
+          </div>
+        </div>
       </div>
     </div>
   );
